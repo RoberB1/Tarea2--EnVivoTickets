beb7e9a53c6e909113008a6cd66528be16fea774
Paulo Tapia <pmtapia@espol.edu.ec>
2025-01-17 22:52:49 -0500

    Package Observer establecido
    .

--- src/sistemaenvivotickets/Observer/Subject.java
+++ src/sistemaenvivotickets/Observer/Subject.java
@@ -1,7 +1,34 @@
 package sistemaenvivotickets.Observer;
 
-public interface Subject {
-    public void agregarObservador(Observer obs);
-    public void eliminarObservador(Observer obs);
-    public void notificarCambios(String msg);
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject implements Observer {
+    private List<Observer> observadores = new ArrayList<>(); 
+    private String nombre; 
+
+    public Subject(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarObservador(Observer obs) {
+        observadores.add(obs);
+    }
+
+    public void eliminarObservador(Observer obs) {
+        observadores.remove(obs);
+    }
+
+    public void notificarCambios(String msg) {
+        for (Observer obs : observadores) {
+            obs.recibirNotificación(msg);
+        }
+    }
+
+    @Override
+    public void recibirNotificación(String msg) {
+        System.out.println("Subject '" + nombre + "' recibió notificación: " + msg);
+        notificarCambios(msg); 
+    }
 }
+
